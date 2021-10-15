96c3034f3732c4ba9d8f7603b457deca8a065b01
Luis Igreja <lfsigreja@gmail.com>
2021-10-15 11:22:13 -0300

    new DB

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'lkz1',
-  password: '1234',
-  database: 'agendamentoquadras',
+  host: 'ec2-18-211-63-235.compute-1.amazonaws.com',
+  username: 'omlizdfhtchxii',
+  password: '2f06f71b8a0a58c0fb2c870307ccac6eddfda23b0931d4f090fe9139cfb4f581',
+  database: 'Joguete',
   define: {
     timestamps: true,
     underscored: true,
